38bc061c296e2aeaa0381332e1c59efd85c9c807
MatToufoutu <mattoufootu@gmail.com>
2011-12-30 00:39:22 +0100

    write the setup.py script

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os.path
+from setuptools import setup, find_packages
+from fatbotslim import NAME, VERSION, AUTHOR
+
+CURRENT_DIR = os.path.dirname(__file__)
+
+
+setup(
+    name = NAME,
+    version = VERSION,
+    description = 'Yet another IRC bot library',
+    long_description = open(os.path.join(CURRENT_DIR, 'README.md')).read().strip(),
+    author = AUTHOR,
+    author_email = 'mattoufootu@gmail.com',
+    url = 'https://github.com/mattoufoutu/fatbotslim',
+    license = 'GPL',
+    keywords = 'irc ircbot bot',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Communications :: Chat :: Internet Relay Chat',
+        'Topic :: Internet',
+    ],
+    packages = find_packages(),
+    include_package_data = True,
+    zip_safe = False,
+    install_requires = open(os.path.join(CURRENT_DIR, 'requirements.txt')).read().strip(),
+    entry_points = """
+    [console_scripts]
+    fatbotslim = fatbotslim.cli:main
+    """
+)

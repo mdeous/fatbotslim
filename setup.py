14d5ec724685b764eef84b615695ed0825da2445
Mathieu Deous <mat.deous@gmail.com>
2013-09-08 02:07:04 +0200

    set development status classifier to Stable

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     license='GPL',
     keywords='irc ircbot bot',
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'Environment :: Plugins',
         'Intended Audience :: Developers',
